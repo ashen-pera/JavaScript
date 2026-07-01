267d58819dca1aad69f98a4904a1f888ccaf6442
Ashen <ashenvihanga757@gmail.com>
2026-07-01 14:36:32 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log("Hello World")
+
+// var x = 3;
+// console.log(x)
+
+// x = 5;
+// console.log(x)
+
+// x = x + 1;
+// console.log(x)
+
+// x = x++;
+// console.log(x)
+
+// var x = 5;
+// console.log(x);
+
+// x = 5+6;
+// console.log(x);
+
+// let sum = x + 8;
+// console.log(sum);
+
